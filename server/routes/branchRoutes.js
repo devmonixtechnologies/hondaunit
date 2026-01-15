135137e40d4fdb6d5f57e0006c897b0db3fcd3f9
DevMonix Technologies <devmonixtechnologies@gmail.com>
2026-01-15 12:57:26 +0530

    Update branchRoutes.js

--- server/routes/branchRoutes.js
+++ server/routes/branchRoutes.js
@@ -1,9 +1,1 @@
-import express from 'express';
 
-import { getBranchesPublic } from '../controllers/branchController.js';
-
-const router = express.Router();
-
-router.get('/', getBranchesPublic);
-
-export default router;
